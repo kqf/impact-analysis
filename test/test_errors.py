d8f6e2e16b52d4d02c320b299f255c50083b1c46
Iohan Petrov <petrov.knu@gmail.com>
2017-07-07 20:28:46 +0200

    Fixed the test error method. The errors are different.

--- test/test_errors.py
+++ test/test_errors.py
@@ -6,6 +6,7 @@ from impact.constants import k_fm, k_norm
 
 import numpy as np
 import unittest
+# from cmath import complex
 
 class TestRealErrors(Configurable):
 
@@ -16,6 +17,9 @@ class TestRealErrors(Configurable):
 
 		self.real_impact = self.data['real_impact_amplitude_error']
 		cov_size = self.data['pdimension']
+
+		self.variable_names = 'a1 a2 a4 b1 b2 b3 b4 a5 b5 b6 a_s rho'.split()
+		self.variables = {v: p for v, p in zip(self.variable_names, self.parameters)}
 
 		# Create some fake covariance matrix
 		cov = [[ 1./(i ** 2 + j + 1) 
@@ -33,8 +37,7 @@ class TestRealErrors(Configurable):
 			import sympy 
 			from sympy import exp as Exp
 			from sympy import sqrt, pi
-			allpars = "t a1 a2 a4 b1 b2 b3 b4 a5 b5 b6 a_s rho"
-			t, a1, a2, a4, b1, b2, b3, b4, a5, b5, b6, a_s, rho =  sympy.symbols(allpars)
+			t, a1, a2, a4, b1, b2, b3, b4, a5, b5, b6, a_s, rho =  sympy.symbols(['t'] + self.variable_names)
 			a_s = a_s / (sqrt(pi *  k_norm) * 4)
 			alpha = (1 - 1j*rho)*(a_s + a4)
 			return 1j*alpha*( a1*Exp(-0.5*alpha*b1*t) + (1 - a1)*Exp(-0.5*alpha*b2*t) ) - 1j*a4*Exp(-0.5*b4*t) - a4*rho/((1 + t/b5)**4)
@@ -51,11 +54,22 @@ class TestRealErrors(Configurable):
 				self.assertAlmostEqual(a, b)
 
 
-	@unittest.skip('t')
+	@unittest.skip('This test fails due to problems with math')
 	def testParameterA1Contribution(self):
-		b1 = [i for i in self.analytic_amplitude.free_symbols if i.name == 'b1'][0]
+		b1 = [i for i in self.analytic_amplitude.free_symbols if i.name == 'a1'][0]
 		partial_derivative = self.analytic_amplitude.diff(b1)
-		# print partial_derivative
-		# TODO: Compare this partial derivative to evaluator.b1 function 
+
+		# TODO: These functions give different results. Check the formula/values by hand
+		# 
+
+		for t in np.linspace(0.2, 10):
+			self.variables['t'] = t
+			analytic = complex(partial_derivative.evalf(subs = self.variables))
+			treuval = self.evaluator.d_a1(t, self.parameters)
+
+			print type(analytic)
+			self.assertAlmostEqual(analytic.real , treuval.real)
+			self.assertAlmostEqual(analytic.imag , treuval.imag)
+
 
 
